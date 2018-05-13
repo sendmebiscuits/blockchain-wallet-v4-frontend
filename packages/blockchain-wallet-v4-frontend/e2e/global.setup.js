53264aceabc0954318fb9bad66818cb4663687c3
schnogz <andrew@blockchain.com>
2018-05-13 17:50:56 -0400

    update startup logging

--- packages/blockchain-wallet-v4-frontend/e2e/global.setup.js
+++ packages/blockchain-wallet-v4-frontend/e2e/global.setup.js
@@ -1,16 +1,19 @@
+const chalk = require('chalk')
 const fs = require('fs')
 const { exec, execSync } = require('child_process')
 
+/* eslint-disable */
 // sometimes the previous server doesnt exit correctly, ensure port is open for tests
+console.log(chalk.red('Killing with \u{1F525}any process running on port 8080...'))
 exec('kill $(lsof -t -i:8080)')
 
 // check if application has already been compiled for production
-/* eslint-disable */
 if (fs.existsSync('./../../dist')) {
-  console.log('Application dist folder found! Continuing...')
+  console.log(chalk.blue('\u{1F680} /dist folder found in root! Prepare for launch... \u{1F680}'))
+  console.log(chalk.red('\u{1F6A8} Reminder: ') + chalk.yellow('If you have changed app source code, you need to rebuild /dist to see changes! \u{1F6A8}'))
   process.exit(0)
 } else {
-  console.log('Application dist folder was not found! Compiling app now...')
+  console.log(chalk.blue('\u{23F3}App /dist folder was not found! Compiling /dist now... \u{231B}'))
 
   return execSync('npm run ci:compile', { cwd: './..', stdio: 'inherit' })
 }
